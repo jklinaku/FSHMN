22be8cdbd0af27f51ea7aa1a7c9e2f2eb285849b
Jon Klinaku <jonklinaku54@gmail.com>
2016-06-05 23:11:52 +0200

    FSHMN

---
+++ Nota.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+public class Nota {
+
+	public static void main(String[] args) {
+	int	i=new Integer(JOptionPane.showInputDialog("Jep piket e marrur ne test")).intValue(), n=i/10;
+	
+		switch(n)
+		{ case 5: { System.out.println("Nota e juaj eshte "+(n+1)); break;}
+		case 6: { System.out.println("Nota e juaj eshte "+(n+1)); break;}
+		case 7: { System.out.println("Nota e juaj eshte "+(n+1)); break;}
+		case 8: { System.out.println("Nota e juaj eshte "+(n+1)); break;}
+		case 9: { System.out.println("Nota e juaj eshte "+(n+1)); break;}
+		
+		}
+
+	}
+
+}
